e0a0931827c29756f984cf6a39326de2a9f92717
Andy Theuninck <andy@gohanman.com>
2017-10-13 13:58:20 -0500

    add failed query logging

--- lib/db.php
+++ lib/db.php
@@ -2,9 +2,21 @@
 
 use COREPOS\common\SQLManager;
 
+class MicroLog
+{
+    public function debug($msg)
+    {
+        $log = __DIR__ . '\\..\\queries.log';
+        $fp = fopen($log, 'a');
+        fwrite($fp, $msg);
+        fclose($fp);
+    }
+}
+
 function db()
 {
-    $dbc = new SQLManager('localhost','MYSQL','meeting','meeting','meeting');
+    $dbc = new SQLManager('localhost','MYSQLI','meeting','meeting','meeting');
+    $dbc->setQueryLog(new MicroLog());
     return $dbc;
 }
 
